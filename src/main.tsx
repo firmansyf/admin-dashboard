084101db819656820678e8f95e88967e436a8a11
Yusuf Firmansyah <yusuffirmansyamh@gmail.com>
2024-05-17 12:42:23 +0700

    fix: fix-routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
 
-import "./index.css";
-import { BrowserRouter } from "react-router-dom";
-import { LayoutControllerProvider } from "./context/index.tsx";
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+import {HashRouter} from 'react-router-dom'
+import {LayoutControllerProvider} from './context/index.tsx'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <LayoutControllerProvider>
         <App />
       </LayoutControllerProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
-);
+)
