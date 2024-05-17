5299bf917e8e4be9c9f910bc94bc74b54500a526
Yusuf Firmansyah <yusuffirmansyamh@gmail.com>
2024-05-17 12:32:00 +0700

    fix: fix-error-carrt

--- src/pages/dashboard/card/SecondCard.tsx
+++ src/pages/dashboard/card/SecondCard.tsx
@@ -1,30 +1,35 @@
 import Chart from 'react-apexcharts'
 import {Card, CardContent, CardHeader} from '@/components/ui/card'
+import {useMemo} from 'react'
+import {ApexOptions} from 'apexcharts'
 
 export default function SecondCard() {
-  const chartState = {
-    series: [44, 55, 13, 43, 22],
-    options: {
-      chart: {
-        width: 380,
-        type: 'pie',
-      },
-      labels: ['Team A', 'Team B', 'Team C', 'Team D', 'Team E'],
-      responsive: [
-        {
-          breakpoint: 480,
-          options: {
-            chart: {
-              width: 200,
-            },
-            legend: {
-              position: 'bottom',
+  const chartState = useMemo(
+    () => ({
+      series: [44, 55, 13, 43, 22],
+      options: {
+        chart: {
+          width: 380,
+          type: 'pie' as const, // Pastikan tipe di sini adalah salah satu dari tipe yang diizinkan
+        },
+        labels: ['Team A', 'Team B', 'Team C', 'Team D', 'Team E'],
+        responsive: [
+          {
+            breakpoint: 480,
+            options: {
+              chart: {
+                width: 200,
+              },
+              legend: {
+                position: 'bottom',
+              },
             },
           },
-        },
-      ],
-    },
-  }
+        ],
+      } as ApexOptions, // Pastikan ini sesuai dengan tipe ApexOptions
+    }),
+    []
+  )
 
   return (
     <section className='flex items-center gap-4 w-full max-sm:flex-col max-lg:flex-row'>
